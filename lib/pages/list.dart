1677a43b4102b7320a1fc7b9abff9e1a80f2c0ef
rusakov <rusakov@wamba.com>
2022-06-14 21:03:32 +0300

    first commit

---
+++ lib/pages/list.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/widgets.dart';
+
+class Book {
+  final int id;
+  final String name;
+  final String desc;
+
+  Book(this.id, this.name, this.desc);
+}
+
+final List<Book> books = [
+  Book(0, "Twillight Sparkle",
+      "Twilight Sparkle is the central main character of My Little Pony Friendship is Magic. She is a female unicorn pony who transforms into an Alicorn and becomes a princess in Magical Mystery Cure"),
+  Book(1, "Starlight Glimmer",
+      "Starlight Glimmer is a female unicorn pony and recurring character, initially an antagonist but later a protagonist, in the series. She first possibly appears in My Little Pony: Friends Forever Issue and first explicitly appears in the season five premiere."),
+  Book(2, "Applejack",
+      "Applejack is a female Earth pony and one of the main characters of My Little Pony Friendship is Magic. She lives and works at Sweet Apple Acres with her grandmother Granny Smith, her older brother Big McIntosh, her younger sister Apple Bloom, and her dog Winona. She represents the element of honesty."),
+  Book(3, "Pinkie Pie",
+      "Pinkie Pie, full name Pinkamena Diane Pie,[note 2] is a female Earth pony and one of the main characters of My Little Pony Friendship is Magic. She is an energetic and sociable baker at Sugarcube Corner, where she lives on the second floor with her toothless pet alligator Gummy, and she represents the element of laughter."),
+  Book(4, "Fluttershy",
+      "Fluttershy is a female Pegasus pony and one of the main characters of My Little Pony Friendship is Magic. She lives in a small cottage near the Everfree Forest and takes care of animals, the most prominent of her charges being Angel the bunny. She represents the element of kindness."),
+];
+
+class BookListPage extends StatelessWidget {
+  
+
+
+  
+  }
